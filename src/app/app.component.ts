9354496b7c844245ea286fcf9c73595bbf0f99a2
Gleb <asd>
2022-04-30 23:37:51 +0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fincult';
+
+  public loan: number = 0;
+  public payment_in_period = 0;
+  public loan_amount: number = 0;
+  public interest_rate: number = 0;
+  public loan_term: number = 0;
+
+  public calculate() {
+    this.loan = this.loan_amount*(this.interest_rate+(this.interest_rate/(1+this.interest_rate)*this.loan_term-1));
+  }
 }
